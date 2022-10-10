da99511ee602f32576f458765a0b554a233ada61
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 01:13:10 +0500

    Добавили продуктов

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
-import './ProductList.css'
+import './ProductList.css';
+import ProductItem from '../ProductItem/ProductItem';
+import { useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const products = [
+    { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
+    { id: '2', title: 'Куртка', price: 5400, description: 'Из натурального дермантина' },
+    { id: '3', title: 'Футболка', price: 700, description: 'С надписью под ваш выбор' },
+    { id: '4', title: 'Кепка', price: 2300, description: 'Разных цветов на выбор' },
+    { id: '5', title: 'Шорты', price: 500, description: 'Для игры в волейбол' },
+    { id: '6', title: 'Брюки', price: 4500, description: 'Для свадьбы самое то' },
+    { id: '7', title: 'Шапка', price: 1500, description: 'Осенне весенняя' },
+    { id: '8', title: 'Бейсболка', price: 1200, description: 'Под цвет ваших глаз' },
+]
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price;
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const { tg } = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        };
+        setAddedItems(newItems);
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams(
+                {
+                    text: `Купить ${getTotalPrice(newItems)}`,
+                }
+            )
+        }
+    };
     return (
-        <div>
-            ProductList
+        <div className={'list'}>
+            {products.map(item => {
+                <ProductItem
+                    key={item.id}
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            })}
         </div>
     );
 };
