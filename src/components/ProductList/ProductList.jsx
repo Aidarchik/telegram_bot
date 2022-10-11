a2a1a34412bba90d5fa8e5d34e2f55bc9ae94e5a
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 12:21:32 +0500

    response

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -34,7 +34,7 @@ const ProductList = () => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(data),
-        });
+        }).then((response) => console.log(response));
     }, [addedItems]);
 
     useEffect(() => {
