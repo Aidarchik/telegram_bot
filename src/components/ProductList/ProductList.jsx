d73e4b0c74235374a335eacc6ef196c8f1142bc0
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 12:27:27 +0500

    dev

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -34,7 +34,9 @@ const ProductList = () => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(data),
-        }).then((response) => console.log(response));
+        }).then((response) => tg.MainButton.setParams({
+            text: response,
+        }));
     }, [addedItems]);
 
     useEffect(() => {
