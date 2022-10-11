66dd8661ca9ac95140aebdea4b19dc7e54141abc
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 11:38:39 +0500

    https на http

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
         };
-        fetch('https://94.26.224.61:3000/web-data', {
+        fetch('http://94.26.224.61:3000/web-data', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(data),
