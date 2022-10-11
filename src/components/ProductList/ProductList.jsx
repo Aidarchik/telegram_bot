eecf5cd72a280855cec076472a9ac62655a6e92c
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 12:29:45 +0500

    Добро

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -35,7 +35,7 @@ const ProductList = () => {
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(data),
         }).then((response) => tg.MainButton.setParams({
-            text: response,
+            text: "Добро",
         }));
     }, [addedItems]);
 
