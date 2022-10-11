4bcc1938ffeab59ddb115ddc5b216bb06ffeb654
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-10-11 11:37:28 +0500

    хук useCallback

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -35,7 +35,7 @@ const ProductList = () => {
             headers: { 'Content-Type': 'application/json', },
             body: JSON.stringify(data),
         });
-    }, []);
+    }, [queryId, addedItems]);
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData);
